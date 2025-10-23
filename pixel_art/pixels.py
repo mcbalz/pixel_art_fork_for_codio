31ba1638f1117a86aa0c29b650926d9ab29e5265
Matt Baldwin <drmatthewcbaldwin@gmail.com>
2025-10-23 12:50:52 +0000

    No idea if this is the right move, modified pixels.py again.

--- pixel_art/pixels.py
+++ pixel_art/pixels.py
@@ -24,13 +24,38 @@ COLORS_DICT = {
 
 
 class Image():
-  def __init__(self, w=7, h=7):
-    self.width = w
-    self.height = h
-    self.img = self.init_list()
+    def __init__(self, w=7, h=7):
+        self.width = w
+        self.height = h
+        self.img = self.init_list()
 
-def init_list(self):
-    lst = []
-    for row in range(self.height):
-        lst.append([])
-    return lst
+    def init_list(self):
+       lst = []
+       for row in range(self.height):
+           lst.append([])
+       return lst
+
+    def disaply(self):
+        for row in self.img:
+            self.display_row(row)
+
+    def display_row(self, row):
+        for pixel in row:
+            print(pixel, end='')
+        print()
+
+    def convert_color(self, str_clr):
+        return COLORS_DICT.get(str_clr, BLACK)
+
+    def add_row(self, *args):
+        if len(args) == 2:
+            colors = [self.convert_color(args[1]) for i in range(self.width)]
+            self.img[args[0]] = colors
+        else:
+            color_params = args[1:]
+            colors = [self.convert_color(arg) for arg in color_params]
+            self.img[args[0]] = colors
+
+
+
+
