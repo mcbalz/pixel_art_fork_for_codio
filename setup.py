6815c0755e5c89149db8280ea99d879d893757b9
Matt Baldwin <drmatthewcbaldwin@gmail.com>
2025-10-23 13:28:39 +0000

    Added the setup.py stuff for packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pixel_art',
+   version='0.1',
+   description='Package for drawing pixel art to the terminal.',
+   author='The Codio Guys (via Matt Baldwin)',
+   email='mcbalz@gmail.com',
+   packages=['pixel_art'],
+   install_requires = ['colorama']
+)
