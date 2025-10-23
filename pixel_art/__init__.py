f7ceebbd2f52625e3cfa0a7a86a179e61879e652
Matt Baldwin <drmatthewcbaldwin@gmail.com>
2025-10-23 12:43:53 +0000

    Made some changes to __init__.py and pixels.py

--- pixel_art/__init__.py
+++ pixel_art/__init__.py
@@ -1,2 +1,4 @@
+from colorama import init, Back, Style
 import pixel_art.examples, pixel_art.pixels
 
+init(autoreset=True)
--- pixel_art/pixels.py
+++ pixel_art/pixels.py
@@ -1,3 +1,36 @@
+from colorama import init, Back, Style
+
+init(autoreset=True)
+
+BLACK = Back.BLACK + '  '
+RED = Back.RED + '  '
+GREEN = Back.GREEN + '  '
+YELLOW = Back.YELLOW + '  '
+BLUE = Back.BLUE + '  '
+MAGENTA = Back.MAGENTA + '  '
+CYAN = Back.CYAN + '  '
+WHITE = Back.WHITE + '  '
+
+COLORS_DICT = {
+    'black' : BLACK,
+    'red': RED,
+    'green': GREEN,
+    'yellow': YELLOW,
+    'blue': BLUE,
+    'magenta': MAGENTA,
+    'cyan': CYAN,
+    'white': WHITE 
+    }
 
 
-# test your code here
+class Image():
+  def __init__(self, w=7, h=7):
+    self.width = w
+    self.height = h
+    self.img = self.init_list()
+
+def init_list(self):
+    lst = []
+    for row in range(self.height):
+        lst.append([])
+    return lst
